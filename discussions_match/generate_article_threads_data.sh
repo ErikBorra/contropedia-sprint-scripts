9c015f1b5195fffcb6554d04be957ed8f0e090a4
Erik Borra <erik@digitalmethods.net>
2014-11-02 20:04:13 +0100

    faster retrieval of revisions, fixed section parsing, fixed encoding issues on osx, discussion files as variables

--- discussions_match/generate_article_threads_data.sh
+++ discussions_match/generate_article_threads_data.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# define locations of input files
+thread_metrics_tree_string='data/top20_thread_metrics_tree_string.csv'
+#discussions_compact_text='data/top20_discussions_compact_text.csv'
+discussions_compact_text='../../WikiTalkParser/discussions/discussions_text.csv'
+thread_titles='data/top20_thread_titles.csv'
+
 # LOAD MYSQL CONFIG
 source db.inc
 
@@ -32,69 +38,36 @@ function download {
   cat "$cache"
 }
 
-# Download the list of all revisions of the page from the API
-rootapiurl="https://en.wikipedia.org/w/api.php?action"
-revs_url="$rootapiurl=query&prop=revisions&titles=$page&rvprop=ids|user|timestamp|comment|sha1&rvlimit=500&rvdir=newer&format=xml&rvstartid="
+# make revisions.tsv
+`sudo rm /tmp/revisions.tsv`
+`echo "SELECT r.id, r.user, r.timestamp, r.hash, r.comment FROM revisions r, article a, article_revisions ar WHERE r.id = ar.revision_id AND ar.article_id = a.id AND a.title = '${page}' INTO OUTFILE '/tmp/revisions.tsv' FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n'" | mysql -u $MYSQLUSER -p$MYSQLPASS $MYSQLDB`
+echo "rev_id\trev_user\trev_timestamp\trev_hash\trev_comment" > "$datadir/revisions.tsv"
+`cat /tmp/revisions.tsv >> $datadir/revisions.tsv`
 
-# cleanup cache for latest revision list
-lastid=0
-for file in $(ls $datadir/.cache/*startid%3D* 2> /dev/null | grep -v ".tmp$"); do
-  revid=$(echo $file | sed 's/^.*startid%3D//')
-  if [ $revid -gt $lastid ]; then
-    lastid=$revid
-  fi
-done
-rm -f "$datadir/.cache/"$(escapeit "$revs_url$lastid")
-
-# Generate a CSV of all revisions from the API data
-run=true
-nextid=0
-pageid=
-echo -e "rev_id\trev_user\trev_timestamp\trev_hash\trev_comment" > "$datadir/revisions.tsv"
-while $run; do
-  download "$revs_url$nextid" | sed 's/<rev/\n<rev/g' > "$datadir/revisions.tmp"
-  if grep '<revisions rvcontinue="' "$datadir/revisions.tmp" > /dev/null; then
-    nextid=$(grep '<revisions rvcontinue="' "$datadir/revisions.tmp" | sed 's/^.*rvcontinue="\([0-9]\+\)".*$/\1/')
-  else
-    run=false
-  fi
-  grep '<rev revid="' "$datadir/revisions.tmp"  |
-    sed 's/^.*revid="//'                        |
-    sed 's/" \/>$//'                            |
-    sed 's/" parentid.*user="/\t/'              |
-    sed 's/" anon="[^"]*"/"/'                   |
-    sed 's/" [^"]\+"/\t/g' >> "$datadir/revisions.tsv"
-  if [ -z "$pageid" ]; then
-    pageid=$(grep '<page pageid="' "$datadir/revisions.tmp" | head -n 1 | sed 's/^.*pageid="\([0-9]\+\)".*$/\1/')
-  fi
-  rm "$datadir/revisions.tmp"
-done
-
-revisions_ids=$(cat "$datadir/revisions.tsv" | grep -v -P "^rev_id\t" | awk -F "\t" '{print $1}' | tr '\n' ' ' | sed 's/ $//')
-revisions_list=$(echo "$revisions_ids" | tr ' ' ',')
+revisions_ids=`sed '1d' $datadir/revisions.tsv | cut -f1`
+revisions_list=$(echo "$revisions_ids" | tr '\n' ',')
 
 # Download list of sections in each revision of the page from the API
 if [ ! -s "$datadir/sections.tsv" ]; then
   rm -f "$datadir/sections.tmp"
   for revid in $revisions_ids; do
     download "https://en.wikipedia.org/w/api.php?action=parse&oldid=$revid&prop=sections|revid&format=json"   |
-      sed 's/","number/\n/g'     |
-      grep -v ']}}'              |
-      sed 's/^.*"line":"//'      |
-      sed 's/^\(.*\)$/\L\1/' >> "$datadir/sections.tmp"
+    	sed -e $'s/","number/\\\n/g' | grep -v ']}}' | sed 's/^.*"line":"//' | sed 's/^\(.*\)$/\1/' >> "$datadir/sections.tmp"
   done
   sort -u "$datadir/sections.tmp" > "$datadir/sections.tsv"
   rm "$datadir/sections.tmp"
 fi
 
-# Get the association of revisions with sections from Eric's database
+# Get the association of revisions with sections from Erik's database
 if [ ! -s "$datadir/revisions_sections.tsv" ]; then
-  echo "SELECT to_revision_id as revision_id, raw_element as section_name FROM element_edit WHERE to_revision_id IN ($revisions_list) GROUP BY to_revision_id, raw_element" | mysql -u $MYSQLUSER -p$MYSQLPASS $MYSQLDB > "$datadir/revisions_sections.tsv"
+	select=$(echo "SELECT to_revision_id as revision_id, raw_element as section_name FROM element_edit WHERE to_revision_id IN ($revisions_list) GROUP BY to_revision_id, raw_element" | sed 's/,)/)/g')
+  echo "$select" | mysql -u $MYSQLUSER -p$MYSQLPASS $MYSQLDB > "$datadir/revisions_sections.tsv"
 fi
 
 # Extract discussions from David's data
-head -n 1 data/top20_discussions_compact_text.csv | iconv -f "iso8859-1" -t "utf8" > "$datadir/discussions.tsv"
-grep -P "^([^\t]+\t){5}$pageid\t" data/top20_discussions_compact_text.csv | iconv -f "iso8859-1" -t "utf8" >> "$datadir/discussions.tsv"
+head -n 1 $discussions_compact_text | iconv -f "iso8859-1" -t "UTF-8" > "$datadir/discussions.tsv"
+grep -P "^([^\t]+\t){5}$pageid\t" $discussions_compact_text | iconv -f "iso8859-1" -t "UTF-8" >> "$datadir/discussions.tsv"
+
 #Add missing thread_title column
 if ! head -n 1 "$datadir/discussions.tsv" | grep -P "\tthread_title$" > /dev/null; then
   ./add_thread_column.sh "$datadir/discussions.tsv" > "$datadir/discussions.tsv.new"
@@ -102,14 +75,14 @@ if ! head -n 1 "$datadir/discussions.tsv" | grep -P "\tthread_title$" > /dev/nul
 fi
 
 # Extract discussions metrics from David's data
-head -n 1 data/top20_thread_metrics_tree_string.csv | iconv -f "iso8859-1" -t "utf8" > "$datadir/threads_metrics.tsv"
-grep -P "^$pageid\t" data/top20_thread_metrics_tree_string.csv | iconv -f "iso8859-1" -t "utf8" >> "$datadir/threads_metrics.tsv"
+head -n 1 $thread_metrics_tree_string | iconv -f "iso8859-1" -t "UTF-8" > "$datadir/threads_metrics.tsv"
+grep -P "^$pageid\t" $thread_metrics_tree_string | iconv -f "iso8859-1" -t "UTF-8" >> "$datadir/threads_metrics.tsv"
 
 # Extract thread permalinks from David's data
-head -n 1 data/top20_thread_titles.csv | iconv -f "iso8859-1" -t "utf8" > "$datadir/threads_links.tsv"
-grep -P "^$pageid\t" data/top20_thread_titles.csv | iconv -f "iso8859-1" -t "utf8" >> "$datadir/threads_links.tsv"
+head -n 1 $thread_titles | iconv -f "iso8859-1" -t "UTF-8" > "$datadir/threads_links.tsv"
+grep -P "^$pageid\t" $thread_titles | iconv -f "iso8859-1" -t "UTF-8" >> "$datadir/threads_links.tsv"
 
-# Extract actors from Eric's database
+# Extract actors from Erik's database
 if [ ! -s "$datadir/actors.tsv" ]; then
   echo "SELECT e.canonical FROM element e LEFT JOIN element_edit ee ON ee.element_id = e.id LEFT JOIN section s ON ee.section_id = s.id LEFT JOIN revisions r ON ee.to_revision_id = r.id LEFT JOIN article_revisions ar ON ar.revision_id = r.id LEFT JOIN article a ON ar.article_id = a.id WHERE a.title = '$page' GROUP BY canonical ORDER BY canonical" | mysql -u $MYSQLUSER -p$MYSQLPASS $MYSQLDB > "$datadir/actors.tsv"
 fi
